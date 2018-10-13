619205431b8545e2b2451be54e8f1f48d3138b8f
jauregui crespo <jauregui.crespo@gmail.com>
2018-10-12 19:19:10 -0500

    contact

--- dev/js/modules/general.js
+++ dev/js/modules/general.js
@@ -1,12 +1,26 @@
-const handleShowElement = (text_id_elem)=>{
-	console.log(text_id_elem);
+const handleShowElement = (text_id_elem) => {
+  console.log(text_id_elem);
 
-	text_id_elem= document.getElementById(text_id_elem);
-	text_id_elem.classList.toggle('active');
+  text_id_elem = document.getElementById(text_id_elem);
+  text_id_elem.classList.toggle('active');
 
 
 }
+$('span#btn-coments').click(function () {
+  $('span#btn-coments').addClass('show');
+  if ($('span#btn-coments').hasClass("show")) {
+    $('form#comments').show('slow');
+  }
+});
 
-export{
-	handleShowElement
+$('p#btn-distribute').click(function () {
+  $('p#btn-distribute').addClass('show');
+  if ($('p#btn-distribute').hasClass("show")) {
+    $('form#distribute').show('slow');
+    $('p#btn-distribute').removeClass('mb-5')
+  }
+});
+
+export {
+  handleShowElement
 }
