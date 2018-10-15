9ea8347c5db6c27d3bfe0ad13cf3b592af43bf65
jauregui crespo <jauregui.crespo@gmail.com>
2018-10-14 22:32:17 -0500

    ab + pro

--- dev/js/index.js
+++ dev/js/index.js
@@ -23,3 +23,85 @@ window.handleItemMenuMapProducts= handleItemMenuMapProducts;
 window.handleItemsMenuTerms= handleItemsMenuTerms;
 
 // console.log('123456');
+$(document).ready(function () {
+    $('.filtering').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: true,
+        slide: 'img'
+    });
+
+    var filtered = false;
+
+    $('.all-filters').on('click', function () {
+        $('.active').removeClass('active');
+        $('.all-filters').addClass('active');
+        $('.filtering').slickUnfilter();
+        filtered = false;
+    });
+
+    $('.mouses-filter').on('click', function () {
+        $('.filtering').slickFilter('.mouses');
+        $('.active').removeClass('active');
+        $('.mouses-filter').addClass('active');
+        filtered = true;
+    });
+
+    $('.computers-filter').on('click', function () {
+        $('.filtering').slickFilter('.computers');
+        $('.active').removeClass('active');
+        $('.computers-filter').addClass('active');
+        filtered = true;
+    });
+
+    $('.pads-filter').on('click', function () {
+        $('.filtering').slickFilter('.pads');
+        $('.active').removeClass('active');
+        $('.pads-filter').addClass('active');
+        filtered = true;
+    });
+
+    $('.leds-filter').on('click', function () {
+        $('.filtering').slickFilter('.leds');
+        $('.active').removeClass('active');
+        $('.leds-filter').addClass('active');
+        filtered = true;
+    });
+    
+    $('.cambiarpestana').click(function(ev){
+        let accion = $(this).data('accion');
+        let cantidadPestana= $('.pills-tab li').length;
+
+        if (accion=='next') {
+            $('.pills-tab li').each(function(index,pill){
+                index= index +1;
+                if ($(pill).find('a').hasClass('active')) {
+                    if (index==cantidadPestana) {
+                        $('.pills-tab li').first().find('a').tab('show');
+                    }
+                    else{
+                        $(pill).next().find('a').tab('show');
+                        return false;
+                    }   
+                    
+                }
+            });
+        }
+        if (accion=='prev') {
+            $.each($('.pills-tab li'),function(index,pill){
+                index= index +1;
+                if ($(pill).find('a').hasClass('active')) {
+                    if (index==1) {
+                        $('.pills-tab li:last-child a').tab('show')
+                        return false;
+                    }
+                    else{
+                        $(pill).prev().find('a').tab('show');
+                        return false;
+                    }  
+                } 
+            });
+        }
+    })
+});
--- dev/js/modules/slick.js
+++ dev/js/modules/slick.js
@@ -27,7 +27,35 @@ const sliderMultipleItems = (name_elem = '.multiple-items', show = 6, autoplay =
     });
   });
 }
+const sliderAbout = (name_elem = '.multiple-items', show = 1, autoplay = false) => {
+  // console.log('slick multiple items');
+  // console.log(name_elem);
+  $(document).ready(function () {
+    $(name_elem).slick({
+      autoplay: autoplay,
+      infinite: true,
+      arrows: true,
+      // speed: 300,
+      slidesToShow: show,
+      slidesToScroll: show
+    });
+  });
+}
+$('.slider-about').slick({
+  autoplay: false,
+  infinite: true,
+  arrows: true,
+  // speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
 
+$('span.origen').on('click', function () {
+  $('.slider-about').slickFilter('.origen');
+  // $('.active').removeClass('active');
+  // $('.origen').addClass('active');
+  
+});
 $('.slide-blog').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
